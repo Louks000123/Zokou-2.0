3d9ecf25c43aa7bed364a32ee14dae178cfdee1d
Loukson <123784461+Loukson@users.noreply.github.com>
2024-09-18 22:48:00 +0000

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 
 
-zokou({nomCom:"bot",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"Louksonbot",reaction:"🍥",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
@@ -30,7 +30,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     })
     .catch(error => {
       console.error('Erreur lors de la traduction en français :', error);
-      repondre('Erreur lors de la traduction en français');
+      repondre('Erreur lors de la traduction en français🌀🤘🏻');
     });
 })
 .catch(error => {
@@ -45,7 +45,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   
 
 
-zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "dalle", reaction: "🍥", categorie: "IA" }, async (dest, zk, commandeOptions) => {
   const { repondre, arg, ms } = commandeOptions;
 
   try {
@@ -58,7 +58,7 @@ zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, c
     const response = await axios.get(`https://vihangayt.me/tools/photoleap?q=${image}`);
     
     const data = response.data;
-    let caption = '*Propulsé par ZOKOU-MD*';
+    let caption = '*Propulsé par NARUTO-MD🍥🤘🏻🌀*';
     
     if (data.status && data.owner && data.data) {
       // Utiliser les données retournées par le service
@@ -73,7 +73,7 @@ zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, c
   }
 });
 
-zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "gpt", reaction: "🍥", categorie: "IA" }, async (dest, zk, commandeOptions) => {
   const { repondre, arg, ms } = commandeOptions;
 
   try {
